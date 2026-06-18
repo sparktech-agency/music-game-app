ed8d8f4ab7a8c4a0df95e600299d5f07eaf7937e
arif-youknow <inbox.arifkhan@gmail.com>
2026-06-18 14:41:30 +0600

    updated

--- lib/core/widgets/app_bar_with_logo.dart
+++ lib/core/widgets/app_bar_with_logo.dart
@@ -1,65 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AppBarWithLogo extends StatelessWidget implements PreferredSizeWidget {
   final String userName;
   final VoidCallback? onTap;
 
-  const AppBarWithLogo({
-    super.key,
-    required this.userName,
-    this.onTap,
-  });
+  const AppBarWithLogo({super.key, required this.userName, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
+      centerTitle: false,
+      titleSpacing: 16,
+      elevation: 0,
       scrolledUnderElevation: 0,
       backgroundColor: const Color(0xFF0A0E21).withValues(alpha: 0.5),
-      elevation: 0,
-      titleSpacing: 0,
-      title: GestureDetector(
+
+      title: InkWell(
         onTap: onTap,
+        borderRadius: BorderRadius.circular(30),
+
         child: Row(
+          mainAxisSize: MainAxisSize.min,
+
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 15, top: 8, bottom: 8),
-              child: SizedBox(
-                width: 36,
-                height: 36,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color(0xFF42E8FF),
-                        Color(0xFF3B82F6),
-                      ],
-                    ),
-                  ),
-                  child: Center(
-                    child: SvgPicture.asset(
-                      'assets/images/logo_only.svg',
-                      colorFilter: const ColorFilter.mode(
-                        Colors.white,
-                        BlendMode.srcIn,
-                      ),
-                      width: 18,
-                      height: 18,
-                    ),
+            Container(
+              width: 36,
+              height: 36,
+
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFF42E8FF), Color(0xFF3B82F6)],
+                ),
+              ),
+
+              child: Center(
+                child: SvgPicture.asset(
+                  'assets/images/logo_only.svg',
+                  width: 18,
+                  height: 18,
+                  colorFilter: const ColorFilter.mode(
+                    Colors.white,
+                    BlendMode.srcIn,
                   ),
                 ),
               ),
             ),
-            const SizedBox(width: 10),
-            Text(
-              userName,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
+
+            const SizedBox(width: 12),
+
+            Flexible(
+              child: Text(
+                userName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ],
