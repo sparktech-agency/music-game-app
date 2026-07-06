fc911b54830fd9c9c8bc6cb2bd163e23f9cf2337
arif-youknow <inbox.arifkhan@gmail.com>
2026-07-06 09:20:25 +0600

    updated

--- lib/features/authentication/data/models/register_model.dart
+++ lib/features/authentication/data/models/register_model.dart
@@ -31,18 +31,21 @@ class RegisterResponseModel extends RegisterEntity {
   });
 
   factory RegisterResponseModel.fromJson(Map<String, dynamic> json) {
+
     final data = json['data'] as Map<String, dynamic>;
 
+    final user = data['user'] as Map<String, dynamic>;
+
     return RegisterResponseModel(
-      id: data['_id'] as String,
-      email: data['email'] as String,
-      role: data['role'] as String,
-      isVerified: data['isVerified'] as bool,
-      firstName: data['firstName'] ?? "Unknown",
-      lastName: data['lastName'] ?? "",
-      nickName: data['nickName'] ?? "user",
-      profile: data['profile'] as String?,
-      createdAt: DateTime.parse(data['createdAt'] as String),
+      id: user['_id'] as String,
+      email: user['email'] as String,
+      role: user['role'] as String,
+      isVerified: user['isVerified'] as bool? ?? false,
+      firstName: user['firstName'] ?? "Unknown",
+      lastName: user['lastName'] ?? "",
+      nickName: user['nickName'] ?? "user",
+      profile: user['profile'] as String?,
+      createdAt: DateTime.parse(user['createdAt'] as String),
     );
   }
 }
