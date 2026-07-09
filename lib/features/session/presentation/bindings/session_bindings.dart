59f34d2946f3cdcf1ec93d6354a8ae4655451730
arif-youknow <inbox.arifkhan@gmail.com>
2026-07-09 15:58:23 +0600

    updated

--- lib/features/session/presentation/bindings/session_bindings.dart
+++ lib/features/session/presentation/bindings/session_bindings.dart
@@ -13,43 +13,37 @@ import 'package:music_game_app/features/session/presentation/controllers/team_na
 import 'package:music_game_app/features/session/presentation/controllers/team_number_controller.dart';
 import 'package:music_game_app/features/session/presentation/controllers/which_team_controller.dart';
 
-class SessionBindings extends Bindings{
-
+class SessionBindings extends Bindings {
   @override
   void dependencies() {
-
-
-
     //============== Data Source ==============
     Get.lazyPut<SessionRemoteSource>(() => SessionRemoteSource(), fenix: true);
-
-
 
     //================= Repositories =================
     Get.lazyPut<SessionRepository>(
           () => SessionRepositoryImpl(Get.find<SessionRemoteSource>()),
+      fenix: true,
     );
-
 
     //================= UseCases =================
 
     Get.lazyPut<CreateSessionUseCase>(
           () => CreateSessionUseCase(Get.find<SessionRepository>()),
+      fenix: true,
     );
 
     Get.lazyPut<StartSessionUseCase>(
           () => StartSessionUseCase(Get.find<SessionRepository>()),
+      fenix: true,
     );
 
     Get.lazyPut<CreateRoundUseCase>(
           () => CreateRoundUseCase(Get.find<SessionRepository>()),
+      fenix: true,
     );
 
+    //================= Central Controller =================
 
-
-
-
-    //================= Controllers =================
     Get.put<CentralSessionController>(
       CentralSessionController(
         Get.find<CreateSessionUseCase>(),
@@ -59,14 +53,13 @@ class SessionBindings extends Bindings{
       permanent: true,
     );
 
-    Get.lazyPut<RoundSelectionController>(()=>RoundSelectionController());
-    Get.lazyPut<TeamNumberController>(()=>TeamNumberController());
-    Get.lazyPut<SingerNumberController>(()=>SingerNumberController());
-    Get.lazyPut<TeamNameController>(()=>TeamNameController());
-    Get.lazyPut<WhichTeamController>(()=>WhichTeamController());
-    Get.lazyPut<PlayerNameController>(()=>PlayerNameController());
+    //================= Step-by-Step Setup Controllers =================
 
+    Get.lazyPut<RoundSelectionController>(() => RoundSelectionController(), fenix: true);
+    Get.lazyPut<TeamNumberController>(() => TeamNumberController(), fenix: true);
+    Get.lazyPut<SingerNumberController>(() => SingerNumberController(), fenix: true);
+    Get.lazyPut<TeamNameController>(() => TeamNameController(), fenix: true);
+    Get.lazyPut<WhichTeamController>(() => WhichTeamController(), fenix: true);
+    Get.lazyPut<PlayerNameController>(() => PlayerNameController(), fenix: true);
   }
-
-
 }
